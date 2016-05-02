8300726f55facab6ad3d7a7ebdf5faeacbaaa43d
boriska70@gmail.com <boriska70@gmail.com>
2016-05-02 22:05:43 +0300

    try CodeDeploy

---
+++ cd_setenvironment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install git
+
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g bower
---
+++ cd_startapp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npm install
+npm start > /dev/null 2> /dev/null < /dev/null &
